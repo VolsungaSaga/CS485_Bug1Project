169ad43adc205aa2eb311de24c7b9271624567fa
JoeBrock <jbrock2@masonlive.gmu.edu>
2016-09-08 22:49:27 -0400

    updated vers of Bug1; still doesn't work, but will work after Tues

--- BugCPP/src/BugAlgorithms.cpp
+++ BugCPP/src/BugAlgorithms.cpp
@@ -1,4 +1,5 @@
 #include "BugAlgorithms.hpp"
+#include <cmath>
 
 BugAlgorithms::BugAlgorithms(Simulator * const simulator)
 {
@@ -6,23 +7,16 @@ BugAlgorithms::BugAlgorithms(Simulator * const simulator)
     //add your initialization of other variables
     //that you might have declared
     
-    //I know that this has to be added otherwise it won't work
-    simulator->ReadObstacles();
-    
     m_mode = STRAIGHT;
     m_hit[0] = m_hit[1] = HUGE_VAL;
     m_leave[0] = m_leave[1] = 0;
     m_distLeaveToGoal = HUGE_VAL;    
+    timesRep = -1;  //For bug1 and bug2
 }
 
 BugAlgorithms::~BugAlgorithms(void)
 {
     //do not delete m_simulator
-}
-
-//Made to cut down on code
-double moveMore(double goal, double curr) {
-    return (goal - curr)/m_simulator->GetDistanceFromRobotToGoal();
 }
 
 Move BugAlgorithms::Bug0(Sensor sensor)
@@ -36,9 +30,7 @@ Move BugAlgorithms::Bug0(Sensor sensor)
   double yGoal = m_simulator->GetGoalCenterY();
 
   //Variables to store future move values.
-  double moveX;
-  double moveY;
-
+  Move moveVector;
 
   if(amITooClose(sensor)){
     //Perpendicular vector of (a,b) = (-b, a) (for left turn)
@@ -46,20 +38,16 @@ Move BugAlgorithms::Bug0(Sensor sensor)
     double xDistObstacle = sensor.m_xmin - xCurr;
     double yDistObstacle = sensor.m_ymin - yCurr;
     Move perpendVector = getPerpendUnitVector(xDistObstacle, yDistObstacle);
-    moveX = perpendVector.m_dx;
-    moveY = perpendVector.m_dy;
-    
+    moveVector = getStepVector(perpendVector.m_dx,perpendVector.m_dy);
   }
 
   else{
-    moveX = moveMore(xGoal,xCurr);
-    moveY = moveMore(yGoal,yCurr);
+    double moveX = (xGoal-xCurr);
+    double moveY = (yGoal-yCurr);
+    moveVector = getStepVector(moveX, moveY);
   }
 
-  Move move ={moveX,moveY};
-  
-
-  return move;
+  return moveVector;
 }
 
 Move BugAlgorithms::Bug1(Sensor sensor)
@@ -73,54 +61,77 @@ Move BugAlgorithms::Bug1(Sensor sensor)
     //goal positions
     double xGoalPos = m_simulator->GetGoalCenterX();
     double yGoalPos = m_simulator->GetGoalCenterY();
-    //Former location and times
-    double xFormLoc;
-    double yFormLoc;
     //Checks best location
-    double xBest = xGoalPos;
-    double yBest = yGoalPos;
-    //Checks best time to go
-    int timesRep = 0;
+    m_hit[0] = xGoalPos;
+    m_hit[1] = yGoalPos;
+    //Checks best time to go 
     //Curr Dist from goal
-    double checkX = |xGoalPos| - |xCurr2|;
-    double checkY = |yGoalPos| - |yCurr2|;
+    double xMove;
+    double yMove;
+    //m_leave 0 == former loc for x; m_leave 1 == former loc for y
+    //m_hit 0 == best x loc; m_hit 1 == best y loc
 
     if (amITooClose(sensor)) {
        //Pretty much go around the circle until you
        //reach back to where you were and then make a note as
        //to the best path possible
-       xBest = xFormLoc = xCurr2;
-       yBest = yFormLoc = yCurr2;
-       if (timesRep == 0) {
-           move[0] = sensor.m_xmin - xCurr;
-           move[1] = sensor.m_ymin - yCurr;
-           if (checkX > (|xGoalPos| - |xCurr2|)
-               xBest = xCurr2Pos;
-           if (checkY > (|yGoalPos| - |yCurr2|))
-               yBest = yCurr2Pos;
-           checkX = |xGoalPos| - |xCurr2|;
-           checkY = |yGoalPos| - |yCurr2|;
+       printf("Print timesRep: %d\n",timesRep); 
+       if (timesRep <= 0) {
+           double senseMoveX = sensor.m_xmin - xCurr2;
+           double senseMoveY = sensor.m_ymin - yCurr2;
+           if (timesRep == -1) {
+	       m_hit[0] = m_leave[0] = senseMoveX;
+	       m_hit[1] = m_leave[1] = senseMoveY;
+	       timesRep += 1;
+	       /*Move perpendVec = getPerpendUnitVector(m_hit[0],m_hit[1]);
+	       move = getStepVector(perpendVec.m_dx,perpendVec.m_dy);
+	       return move;
+	       */
+           } 
+           if (m_hit[0] > (xGoalPos - (sensor.m_xmin-xCurr2)))
+               m_hit[0] = sensor.m_xmin-xCurr2;
+           if (m_hit[1] > (yGoalPos - (sensor.m_ymin-yCurr2)))
+               m_hit[1] = sensor.m_ymin-yCurr2;
+           //perpendVector gives us a straight line
+           Move perpendVec = getPerpendUnitVector(senseMoveX,senseMoveY); 
+           move = getStepVector(perpendVec.m_dx,perpendVec.m_dy);
            //Checks to see if our current locations will ==
-           //our former locations
-           if ((xCurr2 == xFormLoc) && (yCurr2 == yFormLoc))
-               timesRep = 1;
-
-           return move;
+           //our former locations; NOTE: NEEDS WORK
+           /*if (((sensor.m_xmin-xCurr2) == m_leave[0]) && (((sensor.m_ymin-yCurr2) == m_leave[1]))) {
+               timesRep += 1;
+           }*/
+           printf("xForm: %1.2f, yForm: %1.2f, xCurr: %1.2f, yCurr: %1.2f\n",m_leave[0],m_leave[1],(sensor.m_xmin-xCurr2),(sensor.m_ymin-yCurr2));
        }
-       else {
+       else if (timesRep == 1){
            //Here, we make our bug head go to the best vals
-           if ((move[0] <= 0) && (move[1] <= 0)) {
-              moveMore(xGoalPos,xCurr2); 
-              moveMore(yGoalPos,yCurr2);
+           if ((move.m_dx == m_hit[0]) && (move.m_dy == m_hit[1])) {
+               //move in a straight line essentially lol
+               xMove = (xGoalPos - xCurr2);
+               yMove = (yGoalPos - yCurr2);
+               Move perpendVec = getPerpendUnitVector(xMove,yMove);
+               move = getStepVector(perpendVec.m_dx, perpendVec.m_dy);
            }
-           move[0] = xBest-m_xmin;
-           move[1] = yBest-sensor.m_ymin;
+           else {
+              xMove = (sensor.m_xmin-xCurr2);
+              yMove = (sensor.m_ymin-yCurr2);
+              Move perpendVec = getPerpendUnitVector(xMove,yMove); 
+              move = getStepVector(perpendVec.m_dx,perpendVec.m_dy);
+           }
+           return move;
        }
     }
     else {
-       //move in a straight line essentially lol
-       moveX = moveMore(xGoalPos,xCurr2);
-       moveY = moveMore(yGoalPos,yCurr2);
+        //move in a straight line essentially lol
+        if (timesRep < 0) {
+            xMove = (xGoalPos - xCurr2);
+            yMove = (yGoalPos - yCurr2);
+        }
+        else {
+            //This code is our circle
+            xMove = sensor.m_xmin - xCurr2;
+            yMove = sensor.m_ymin - yCurr2;
+        }
+        move = getStepVector(xMove, yMove);
     }
 
     return move;
@@ -146,8 +157,25 @@ bool BugAlgorithms::amITooClose(Sensor sensor)
   return false;
 }
 
+//Code for getCircleVector experimental; going to circle around an obj
+//Move BugAlgorithms::getCircleVector(double x, double y) {}
+
 //Calculates the left-turning vector for a given vector (like the nearest
 //obstacle vector, for instance) in the following way:
+
+Move BugAlgorithms::getStepVector(double x, double y){
+  double stepSize = (m_simulator->GetStep());
+  double originalMagnitude = getMagnitude(x,y);
+
+  double stepX = (stepSize/originalMagnitude) * x;
+  double stepY = (stepSize/originalMagnitude) * y;
+
+  Move stepVector = {stepX, stepY};
+
+  return stepVector;
+
+
+}
 
 //X_perp = -y/|vector|
 //Y_perp = x/|vector|
@@ -171,24 +199,5 @@ double BugAlgorithms::getMagnitude(double x, double y){
   return sqrt((x * x)+(y * y));
 }
        
-//From the given x and y components of a vector, calculate the
-// vector of magnitude 0.06 that
-// goes in the same direction. That is:
 
-// U = ((L^2)/|V|) * V , U = vector of magnitude L, V = given vector.
-
-
-Move BugAlgorithms::getStepVector(double x, double y){
-  double stepSize = (m_simulator->GetStep());
-  double originalMagnitude = getMagnitude(x,y);
-
-  double stepX = (stepSize/originalMagnitude) * x;
-  double stepY = (stepSize/originalMagnitude) * y;
-
-  Move stepVector = {stepX, stepY};
-
-  return stepVector;
-
-
-}
 
