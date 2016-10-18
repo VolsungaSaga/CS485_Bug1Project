919a0db70cd53b2f3a7e166e091e12976dc75a18
joshua <joshualilly@me.com>
2016-10-17 17:33:27 -0700

    Adding the jacobian function

--- assignmentII/Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.hpp
+++ assignmentII/Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.hpp
@@ -30,9 +30,38 @@ public:
      *
      */
     RigidBodyMove ConfigurationMove(void);
-    
+
+    /*!
+        Multiply the point x, y, theta by the jacobian. x, y should be a control point
+            in the world space
+        \param[in] p_retVec - the 1x3 return vector - this holds [m_dx, m_dy, m_dtheta]
+            note don't new this from the heap just create it on the stack since it is a temporary.
+        \param[in] p_x - the x coordinate in the world space, x control point.
+        \param[in] p_y - the y coordinate in the world space, y control point.
+        \param[in] p_theta - the world space configuration angle. - ***Should be in radians!!!
+        \param[in] p_scaleXY the scale factor for the x, y point.
+        \param[in] p_scaleTheta - the scale factor for theta. 
+     */
+    void worldSpaceToConfigSpace(std::vector<double> p_retVector, double p_xControl, double p_yControl,
+        double p_theta, double DUx, double DUy, double p_scaleXY, double p_scaleTheta) const
+    {
+        // the first two values of our vector dx and dy are simply x and y.
+        p_retVector.push_back(p_scaleXY * DUx);
+        p_retVector.push_back(p_scaleXY * DUy);
+        
+        // first part of the matrix i.e. -X_j * sin(theta) - Y_j * cos(theta) * DUx
+        double accumulator = -p_xControl * std::sin(p_theta) - p_yControl * std::cos(p_theta) * DUx;
+
+        // accumulator now has (-X_j * sin(theta) - Y_j * cos(theta) * DUx) +
+        //                     (X_j * cos(theta) - Y_j * sin(theta) * DUy)
+        accumulator += p_xControl * std::cos(p_theta) - p_yControl * std::sin(p_theta) * DUy;
+        p_retVector.push_back(p_scaleTheta * accumulator);
+         
+    }
+     
 protected:
     RigidBodySimulator *m_simulator;
+
 };
 
 #endif
