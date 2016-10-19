d2837244a70435b05899fb28bcee2b4ce3058638
joshua <joshualilly@me.com>
2016-10-18 19:48:31 -0700

    Adding code for robot motion it is still broken

--- assignmentII/Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.cpp
+++ assignmentII/Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.cpp
@@ -1,5 +1,5 @@
 #include "RigidBodyPlanner.hpp"
-
+#include <iostream>
 RigidBodyPlanner::RigidBodyPlanner(RigidBodySimulator * const simulator)
 {
     m_simulator = simulator;   
@@ -10,14 +10,80 @@ RigidBodyPlanner::~RigidBodyPlanner(void)
     //do not delete m_simulator  
 }
 
+const std::vector<double> RigidBodyPlanner::getDifferentialVector() const
+{
+  // calculate the attractive forces for x and y
+  double x_att = 0;
+  double y_att = 0;
+
+  x_att = m_simulator->GetRobotX() - m_simulator->GetGoalCenterX(); // x = robotx - goalx
+  y_att = m_simulator->GetRobotY() - m_simulator->GetGoalCenterY(); // y = roboty - goaly
+
+  // these will hold the running sum
+  double repulsiveX = 0;
+  double repulsiveY = 0;
+
+  const double* verticies = m_simulator->GetRobotVertices();
+  // for repulsive force we must sum all control points with all obsticles for x and y components.
+  for (int i = 0; i < 2 * m_simulator->GetNrRobotVertices(); i+=2) // 2 * since verticy is (x, y)
+  {
+    // for every control point
+    double controlX = *(verticies + i);
+    double controlY = *(verticies + (i + 1));
+    
+    // calculate the repulsive force for each obsticle.
+    for (int j = 0; j < m_simulator->GetNrObstacles(); ++j)
+    {
+      // get the closest point on the obsticle to this control point
+      Point closest = m_simulator->ClosestPointOnObstacle(j, controlX, controlY);
+
+      // repulsive force is the sum over all obsticles and all points.
+      repulsiveX = repulsiveX + (closest.m_x - controlX);
+      repulsiveY = repulsiveY + (closest.m_y - controlY);
+    }
+  }
+
+  // since it's only length two it's okay to return a copy.
+  std::vector<double> differential(2);
+  differential[0] = (x_att + repulsiveX);
+  differential[1] = (y_att + repulsiveY);
+
+  // need to make a unit vector
+  double magnitude = std::sqrt((differential[0] * differential[0]) + 
+    (differential[1] * differential[1]));
+  differential[0] /= magnitude;
+  differential[1] /= magnitude;
+  return differential;
+}
 
 RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
-    
 {
     RigidBodyMove move;
+    double controlX = 0;
+    double controlY = 0;
+    double theta = 0;
+    std::vector<double> configSpace(3);
+    std::vector<double> differentialVector(3);
+    std::vector<double> moveAccumulator(3);
 
-//add your implementation
+    const double* verticies = m_simulator->GetRobotVertices();
+    for (int i = 0; i < 2 * m_simulator->GetNrRobotVertices(); i+=2)
+    {
+      differentialVector = getDifferentialVector();
+      controlX = *(verticies + i);
+      controlY = *(verticies + (i + 1));
+      theta = m_simulator->GetRobotTheta();
+  
+      worldSpaceToConfigSpace(configSpace, controlX, controlY, theta, 
+        differentialVector[0], differentialVector[1], 0.1, 0.1);    
 
+      move.m_dx += configSpace[0];
+      move.m_dy += configSpace[0];
+      move.m_dtheta += configSpace[0];
+    }
+    move.m_dx = move.m_dx * -1;
+    move.m_dx = move.m_dy * -1;
+    move.m_dx = move.m_dtheta * -1;
     return move;
 }
 
--- assignmentII/Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.hpp
+++ assignmentII/Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.hpp
@@ -42,12 +42,12 @@ public:
         \param[in] p_scaleXY the scale factor for the x, y point.
         \param[in] p_scaleTheta - the scale factor for theta. 
      */
-    void worldSpaceToConfigSpace(std::vector<double>& p_retVector, const double& p_xControl, const double& p_yControl,
+    inline void worldSpaceToConfigSpace(std::vector<double>& p_retVector, const double& p_xControl, const double& p_yControl,
         const double& p_theta, const double& DUx, const double& DUy, const double& p_scaleXY, const double& p_scaleTheta) const
     {
         // the first two values of our vector dx and dy are simply x and y.
-        p_retVector.push_back(p_scaleXY * DUx);
-        p_retVector.push_back(p_scaleXY * DUy);
+        p_retVector[0] = (p_scaleXY * DUx);
+        p_retVector[1] = (p_scaleXY * DUy);
         
         // first part of the matrix i.e. -X_j * sin(theta) - Y_j * cos(theta) * DUx
         double accumulator = -p_xControl * std::sin(p_theta) - p_yControl * std::cos(p_theta) * DUx;
@@ -55,7 +55,7 @@ public:
         // accumulator now has (-X_j * sin(theta) - Y_j * cos(theta) * DUx) +
         //                     (X_j * cos(theta) - Y_j * sin(theta) * DUy)
         accumulator += p_xControl * std::cos(p_theta) - p_yControl * std::sin(p_theta) * DUy;
-        p_retVector.push_back(p_scaleTheta * accumulator);
+        p_retVector[2] = (p_scaleTheta * accumulator);
     }
      
   //Helper Functions - Mostly having to do with point and matrix arithmetic.
@@ -64,9 +64,12 @@ public:
   Point pointSubtract(Point p1, Point p2);
   Point pointMultiply(Point p1, Point p2);
 
-  // std::vector<std::vector<double>> matrixAdd(std::vector<std::vector<double>> m1, std::vector<std::vector<double>> m2);
-  // std::vector<std::vector<double>> matrixMultiply(std::vector<vector> m1, std::vector<vector> m2);
-    
+  /*!
+    Get a 1x2 vector representing the differential x, y attractive
+      and repulsive fources.
+   */
+  const std::vector<double> getDifferentialVector() const;
+ 
 protected:
   RigidBodySimulator *m_simulator;
 };
