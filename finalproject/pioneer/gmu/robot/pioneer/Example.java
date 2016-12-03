d7572bd489785c5706973cc36358eab242f8fa70
wangfarmer <johnwangcq@gmail.com>
2016-12-03 15:38:28 -0500

    Update Example.java

--- finalproject/pioneer/gmu/robot/pioneer/Example.java
+++ finalproject/pioneer/gmu/robot/pioneer/Example.java
@@ -1,7 +1,3 @@
-/*
- * Example.java
- */
-
 package gmu.robot.pioneer;
 
 /**
@@ -16,93 +12,108 @@ package gmu.robot.pioneer;
 
 public class Example {
 
-	public static void usage() throws Exception {
-		System.out.println("Usage: Example <port>");
-		// System.exit(0);
-		String[] derp = { "5000" };
-		main(derp);
-	}
+  
+ static double goalX = -200;
+ static double goalY = 700;
+ 
+ public static void usage() throws Exception {
+  System.out.println("Usage: Example <port>");
+  // System.exit(0);
+  String[] derp = { "5000" };
+  main(derp);
+ }
 
-	public static final void main(String[] args) throws Exception {
-		if (args.length < 1)
-			usage();
+ public static final void main(String[] args) throws Exception {
+  if (args.length < 1)
+   usage();
 
-		PioneerRobot robot = new PioneerRobot();
-		robot.setVerbose(true);
-		robot.connect("127.0.0.1", Integer.parseInt(args[0]));
-		robot.sonar(true);
-		robot.enable(true);
-		robot.setVerbose(true);
-		double goalX = 1000;
-		double goalY = 1000;
-		int state = 1;
-		boolean check = true;
+  PioneerRobot robot = new PioneerRobot();
+  robot.setVerbose(true);
+  robot.connect("127.0.0.1", Integer.parseInt(args[0]));
+  robot.sonar(true);
+  robot.enable(true);
+  robot.setVerbose(true);
+  
+  int state = 1;
+  boolean check = true;
 
-		try {
-			Thread.sleep(5000);
-		} catch (Exception e) {
-		}
+  try {
+   Thread.sleep(5000);
+  } catch (Exception e) {
+  }
 
-		MedianFilter filter = new MedianFilter(robot);
+  //MedianFilter filter = new MedianFilter(robot);
 
-		while (check) {
+  while (check) {
 
-			switch (state) {
-			case 0: // reached the goal state
-				check = false;
-				break;
-			case 1: // move all toward the goal
-				double angleRads = Math.atan(Math.abs(goalY - robot.getYPos()) / 
-						 Math.abs(goalX - robot.getXPos()));
-				
-				System.out.println("subtraction " + Math.abs(robot.getOrientation() - angleRads));
-				System.out.println("angle calc " + angleRads);
-				System.out.println("Robot Or " + robot.getOrientation());
+   switch (state) {
+   case 0: // reached the goal state
+    check = false;
+    break;
+   case 1: // move all toward the goa
+    double angleRads = Math.atan(Math.abs(goalY - robot.getYPos()) / 
+       Math.abs(goalX - robot.getXPos()));
+    
+    System.out.println("subtraction " + Math.abs(robot.getOrientation() - angleRads));
+    System.out.println("angle calc " + angleRads);
+    System.out.println("Robot Or " + robot.getOrientation());
 
-		//		if (Math.abs(robot.getOrientation() - angleRads) > 0.01) {
-					
-					double angleDeg = Math.toDegrees(angleRads);
-					// first or second quadrant.
-					if ((goalX >= 0 && goalY >= 0)) {
-						System.out.println("First quad");
-						robot.dhead((short) angleDeg);
-					} else if ((goalX < 0 && goalY > 0)) {
-						System.out.println("second quad");
-						robot.dhead((short) (180 - angleDeg));
-					} else if (goalX < 0 && goalY < 0) {
-						System.out.println("third quad");
-						robot.dhead((short) (180 + angleDeg));
-					} else if (goalX > 0 && goalY < 0) {
-						System.out.println("forth quad");
-						robot.dhead((short) (360 - angleDeg));
-					}
-					// Thread.sleep(1000);
-		//		} else {
-					while (!robot.isValidData())
-					{
-						System.out.println("not valid");
-					}
-					robot.vel2((byte) 5, (byte) 5);
-	//			}
-				
-				if (robot.getXPos() > goalX && robot.getYPos() > goalY) {
-					state = 0;
-				}
-			}
+     doRotate(robot, angleRads);
+     
 
-		}
-		robot.e_stop();
+     robot.vel2((byte) 5, (byte) 5);
+    
+    if (Math.abs(robot.getXPos()) > Math.abs(goalX) && Math.abs(robot.getYPos()) > Math.abs(goalY)) {
+       System.out.println(robot.getXPos());
+       System.out.println(robot.getYPos());
+      state = 0;
+    }
+   }
 
-		try {
-			Thread.sleep(2000);
-		} catch (Exception e) {
-		}
-		System.out.println(robot.getXPos());
+  }
 
-		/*
-		 * for( short i = 0 ; i < 10 ; i++ ) { robot.sound(i); robot.dhead(
-		 * (short)(60 * (short)(2*(i%2)-1)) ); Thread.sleep(1000); }
-		 */
-		robot.disconnect();
-	}
+  robot.e_stop();
+  
+  try {
+   Thread.sleep(2000);
+  } catch (Exception e) {
+  }
+
+  /*
+   * for( short i = 0 ; i < 10 ; i++ ) { robot.sound(i); robot.dhead(
+   * (short)(60 * (short)(2*(i%2)-1)) ); Thread.sleep(1000); }
+   */
+  robot.disconnect();
+ }
+ 
+ public static void doRotate(PioneerRobot robot, double angleRads){
+   //rotate and go to the goal
+        double angleDeg = Math.toDegrees(angleRads);
+     // first or second quadrant.
+     if ((goalX >= 0 && goalY >= 0)) {
+      System.out.println("First quad");
+      for(int i=0; i<100;i++){
+        robot.head((short) angleDeg);
+      }
+      
+     } else if ((goalX < 0 && goalY > 0)) {
+      System.out.println("second quad");
+      
+      for(int i=0; i<100;i++){
+        robot.head((short) (180 - angleDeg));
+      }
+     } else if (goalX < 0 && goalY < 0) {
+      System.out.println("third quad");
+     
+      for(int i=0; i<100;i++){
+         robot.head((short) (180 + angleDeg));
+      }
+     } else if (goalX > 0 && goalY < 0) {
+      System.out.println("forth quad");
+     
+      for(int i=0; i<100;i++){
+        robot.head((short) (360 - angleDeg));
+      }
+     }
+ }
 }
